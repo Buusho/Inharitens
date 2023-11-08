d98828bea993daeef88cee626185bcfc68f4bd9b
Adyl < >
2023-11-08 19:38:47 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+Programmer programmer = new Programmer("Adyl","young","Apple" );
+        System.out.println(programmer);
+programmer.coding();
+programmer.eat();
+programmer.walk();
+programmer.learn();
+
+
+Dancer dancer = new Dancer("Abu","old","CS2");
+        System.out.println(dancer);
+dancer.dencing();
+dancer.eat();
+dancer.walk();
+dancer.learn();
+
+
+
+
+Singer singer = new Singer("Ainazik","young","BlackPink");
+        System.out.println(singer);
+singer.singing();
+singer.playGitar();
+singer.eat();
+singer.walk();
+singer.learn();
+
+
+    }
+}
